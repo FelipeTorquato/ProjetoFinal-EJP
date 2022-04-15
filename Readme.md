14c1cbb580ab1256f773ada686108fb7a298713d
FelipeTorquato <felipectorquato1@gmail.com>
2022-04-15 19:50:34 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Projeto Final - Escola Jovens Programadores
+
+Sistema de Otimização de Estoque - BinSys
+
+O programa tem a finalidade de auxiliar no processo de gerenciamento de um estoque, indicando quando certo produto está em estoque crítico (10% do total).
+
+Ainda em versão inicial, sem conexão com banco de dados e sem alguns recursos.
---
+++ src/main/java/com/mycompany/projetoejpj/ProjetoEJPJ.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetoejpj;
+
+import Telas.TelaSplash;
+
+/**
+ *
+ * @author felip
+ */
+public class ProjetoEJPJ {
+    public static void main(String[] args) {
+        new TelaSplash().setVisible(true);
+    }
+}
